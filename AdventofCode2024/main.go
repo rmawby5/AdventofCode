7fe57869cc9053debe2d2ea0727e52a2fcef9715
Ryan <rmawby52@gmail.com>
2024-12-03 23:00:10 +0000

    Add Pretty-Table to main

--- AdventofCode2024/main.go
+++ AdventofCode2024/main.go
@@ -1,20 +1,23 @@
 package main
 
 import (
-	"fmt"
+	"os"
 
 	"Aoc.com/AdventOfCode2024/day3"
+	"github.com/jedib0t/go-pretty/v6/table"
 )
 
 func main() {
-	//day3.Part2()
-	Parse1Time, P1Time, P1Total := day3.Part2()
-	fmt.Printf("Parse Time: %v ", Parse1Time)
-	fmt.Printf("Part 1 Time: %v ", P1Time)
-	fmt.Printf("Part 1 Total: %d\n", P1Total)
-	Parse2Time, P2Time, P2Total := day3.Part2()
-	fmt.Printf("Parse Time: %v ", Parse2Time)
-	fmt.Printf("Part 2 Time: %v ", P2Time)
-	fmt.Printf("Part 2 Total: %d ", P2Total)
 
+	Parse1Time, P1Time, P1Total := day3.Part1()
+	Parse2Time, P2Time, P2Total := day3.Part2()
+	t := table.NewWriter()
+	t.SetOutputMirror(os.Stdout)
+	t.AppendHeader(table.Row{"Part", "Parse Time", "Part Time", "Result"})
+	t.AppendRows([]table.Row{
+		{1, Parse1Time, P1Time, P1Total},
+	})
+	t.AppendSeparator()
+	t.AppendRow([]interface{}{2, Parse2Time, P2Time, P2Total})
+	t.Render()
 }
