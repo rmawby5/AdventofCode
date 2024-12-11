19e2e0c7c77604b6ebc4a03682823155fb682e4f
Ryan <rmawby52@gmail.com>
2024-12-11 23:27:41 +0000

    clean d11 p1, add p2

--- AdventofCode2024/day11/day11.go
+++ AdventofCode2024/day11/day11.go
@@ -8,69 +8,100 @@ import (
 	fileparse "Aoc.com/AdventOfCode2024/FileParse"
 )
 
-
-
-
-func splitter(full int)(int, int){
+func splitter(full int) (int, int) {
 	//takes int value, returns two ints with left and right half of input's digits
-	char,err = strconv.Itoa(full)
+	char := strconv.Itoa(full)
 	left := char[:len(char)/2]
 	right := char[len(char)/2:]
-	leftI,_ := strconv.Atoi(left)	
-	rightI,_ := strconv.Atoi(right)
-	return leftI,rightI
+	leftI, _ := strconv.Atoi(left)
+	rightI, _ := strconv.Atoi(right)
+	return leftI, rightI
 }
 
 func lenInt(i int) bool {
-    if i >= 1e18 {
-        return 19
-    }
-    x, count := 10, 1
-    for x <= i {
-        x *= 10
-        count++
-    }
-	if count%2 == 0{
-		return true
+	if i >= 1e18 {
+		return false
 	}
-    return false
-}
-func blinker(input int, blinkCount int)int{
-	stones := 0
-	//case 0
-	if blinkCount == 25{
-		stones = 1
-	}else{
-		if input == 0{
-			stones += blinker(1,blinkCount+1)
-		}else if lenInt(input){										//check function
-			l,r := splitter(input)
-			stones+= blinker(l, blinkCount+1)
-			stones+=blinker(r,blinkCount+1)
-		}else{
-			stones+= blinker(input*2024, blinkCount+1)
-		}
+	x, count := 10, 1
+	for x <= i {
+		x *= 10
+		count++
 	}
-	return stones
+	return count%2 == 0
+
 }
 
-func Part1() (time.Duration, time.Duration, int64) {
+func blinker(input int, blinkCount int, blinkLimit int, cache map[string]int) int {
+	//stones := 0
+	//case 0
+	n := strconv.Itoa(input) + "," + strconv.Itoa(blinkCount)
+	if cache[n] == 0 {
+		if blinkCount == blinkLimit {
+			cache[n] = 1
+		} else {
+			if input == 0 {
+				cache[n] = blinker(1, blinkCount+1, blinkLimit, cache)
+			} else if lenInt(input) { //check function
+				l, r := splitter(input)
+				cache[n] = blinker(l, blinkCount+1, blinkLimit, cache) + blinker(r, blinkCount+1, blinkLimit, cache)
+				//stones += blinker(r, blinkCount+1,cache)
+			} else {
+				cache[n] = blinker(input*2024, blinkCount+1, blinkLimit, cache)
+			}
+		}
+	}
+
+	return cache[n]
+}
+
+func Part1() (time.Duration, time.Duration, int) {
+	cache := make(map[string]int)
 	ParseStart := time.Now()
-	raw := fileparse.FileParse("day10/TestInput.txt")
+	raw := fileparse.FileParse("day11/Input.txt")
 	ParseTime := time.Since(ParseStart)
 	P1Start := time.Now()
 	//Insert additional processsing here
 	var in []int
-	for _,i:= range raw{
-		in = strings.Split(i,"")
+	for _, i := range raw {
+		tmp := strings.Split(i, " ")
+		for _, j := range tmp {
+			p, _ := strconv.Atoi(j)
+			in = append(in, p)
+		}
 	}
 	//inerst Puzzle solution here
 	stoneCount := 0
-	for _,k := range in {
-		conv,_ := strconv.Atoi(k)
-		stoneCount+= blinker(conv, )
-		
+	//fmt.Println(in)
+	for _, k := range in {
+		stoneCount += blinker(k, 0, 25, cache)
+
 	}
 	P1Time := time.Since(P1Start)
 	return ParseTime, P1Time, stoneCount
+}
+
+func Part2() (time.Duration, time.Duration, int) {
+	cache := make(map[string]int)
+	ParseStart := time.Now()
+	raw := fileparse.FileParse("day11/Input.txt")
+	ParseTime := time.Since(ParseStart)
+	P2Start := time.Now()
+	//Insert additional processsing here
+	var in []int
+	for _, i := range raw {
+		tmp := strings.Split(i, " ")
+		for _, j := range tmp {
+			p, _ := strconv.Atoi(j)
+			in = append(in, p)
+		}
+	}
+	//inerst Puzzle solution here
+	stoneCount := 0
+	//fmt.Println(in)
+	for _, k := range in {
+		stoneCount += blinker(k, 0, 75, cache)
+
+	}
+	P2Time := time.Since(P2Start)
+	return ParseTime, P2Time, stoneCount
 }
