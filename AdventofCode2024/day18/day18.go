c468e7a752c2d75d1f3e63e3b178a121b92ce70b
rmawby5 <153398487+rmawby5@users.noreply.github.com>
2024-12-19 16:53:46 +0000

    Draft for d19

--- AdventofCode2024/day18/day18.go
+++ AdventofCode2024/day18/day18.go
@@ -137,3 +137,87 @@ func Part2() (time.Duration, time.Duration, string) {
 	P2Time := time.Since(P2Start)
 	return ParseTime, P2Time, failByte
 }
+
+
+
+
+
+
+
+
+
+
+/*
+package main
+
+import (
+	"slices"
+	"strings"
+	"time"
+
+	fileparse "Aoc.com/AdventOfCode2024/FileParse"
+)
+
+func isValidArrangement(arrangement []string, towels []string, bufLen int) bool {
+	valid := false
+	buffer = ""
+	char := 0
+
+	for len(buffer) < bufLen {
+		buffer = buffer + arrangement[char]
+		if slices.Contains(towels, buffer) {
+			//valid towel found
+			//check for end of arrangement
+			if len(buffer) == len(arrangement) {
+				return true
+			}
+			valid = isValidArrangement(arrangement[len(buffer):], towels, bufLen)
+			if valid {
+				return valid
+			}
+		} else {
+			if len(buffer) == len(arrangement) {
+				return false
+			}
+		}
+	}
+
+	return valid
+}
+
+func Part1() (time.Duration, time.Duration, int) {
+	ParseStart := time.Now()
+	raw := fileparse.FileParse("day19/TestInput.txt")
+	towelRaw := fileparse.FileParse("day19/TestTowels.txt")
+	ParseTime := time.Since(ParseStart)
+	P1Start := time.Now()
+
+	//Insert additional processsing here
+	input := make([][]string, len(raw))
+	for _, i := range raw {
+		input = append(input, strings.Split(i, ""))
+	}
+	tmp := ""
+	for _, i := range towelRaw {
+		tmp = tmp + i
+	}
+	towels = strings.Split(tmp, ", ")
+	maxLen := 0 //maximum number of stripes on a given towel
+	for _, i := range towels {
+		if len(i) > maxlen {
+			maxLen = len(i)
+		}
+	}
+
+	//insert Puzzle solution here
+	sum := 0
+	for _, i := range input {
+		if isValidArrangement(i, towels, maxLen) {
+			sum++
+		}
+	}
+
+	P1Time := time.Since(P1Start)
+	return ParseTime, P1Time, sum
+}
+*/
