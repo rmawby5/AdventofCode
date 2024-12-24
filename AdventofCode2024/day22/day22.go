35c9342f9fd08c9264f2fd96cfb53dcc518a7b5d
Ryan <rmawby52@gmail.com>
2024-12-24 23:10:07 +0000

    add d24 P2

--- AdventofCode2024/day22/day22.go
+++ AdventofCode2024/day22/day22.go
@@ -1,7 +1,7 @@
 package day22
 
 import (
-	"fmt"
+	"slices"
 	"strconv"
 	"time"
 
@@ -18,30 +18,43 @@ func secret(seed int) int {
 	return start
 }
 
-func lastDigit(in int) int {
-	st := strconv.Itoa(in)
-	last := st[len(st)-1:]
-	fmt.Println(last)
-	lI, _ := strconv.Atoi(last)
-	return lI
+func stringSquash(buffer []int) string {
+	str := ""
+	for _, i := range buffer {
+		str = str + strconv.Itoa(i) + ","
+	}
+	return str
 }
-
-func price(seed int) int {
+func price(seed int, cache map[string]int) ([]int, map[string]int) {
 	start := seed
 	var change []int
+	seq := ""
+	var visited []string
+
 	for i := 0; i < 2000; i += 1 {
+		b := start % 10
 		start = (((start * 64) ^ start) % 16777216)
 		start = (((start / 32) ^ start) % 16777216)
 		start = (((start * 2048) ^ start) % 16777216)
-		change = append(change, lastDigit(start))
+		change = append(change, start%10-b)
+		if len(change) == 5 {
+			change = change[1:]
+			seq = stringSquash(change)
+			if cache[seq] == 0 {
+				cache[seq] = start % 10
+				visited = append(visited, seq)
+			} else if !slices.Contains(visited, seq) {
+				cache[seq] = cache[seq] + start%10
+				visited = append(visited, seq)
+			}
+		}
 	}
-
-	return start
+	return change, cache
 }
 
 func Part1() (time.Duration, time.Duration, int) {
 	ParseStart := time.Now()
-	raw := fileparse.FileParse("day22/TestInput.txt")
+	raw := fileparse.FileParse("day22/Input.txt")
 	ParseTime := time.Since(ParseStart)
 	P1Start := time.Now()
 
@@ -64,7 +77,7 @@ func Part1() (time.Duration, time.Duration, int) {
 
 func Part2() (time.Duration, time.Duration, int) {
 	ParseStart := time.Now()
-	raw := fileparse.FileParse("day22/TestInput.txt")
+	raw := fileparse.FileParse("day22/Input.txt")
 	ParseTime := time.Since(ParseStart)
 	P2Start := time.Now()
 
@@ -77,10 +90,16 @@ func Part2() (time.Duration, time.Duration, int) {
 
 	//insert Puzzle solution here
 	sum := 0
+	cache := make(map[string]int)
 	for _, i := range input {
-		sum += secret(i)
+		_, cache = price(i, cache)
 	}
 
+	for _, ban := range cache {
+		if ban > sum {
+			sum = ban
+		}
+	}
 	P2Time := time.Since(P2Start)
 	return ParseTime, P2Time, sum
 }
--- AdventofCode2024/go.mod
+++ AdventofCode2024/go.mod
@@ -6,10 +6,12 @@ require github.com/jedib0t/go-pretty/v6 v6.6.3
 
 require (
 	github.com/patrickmn/go-cache v2.1.0+incompatible // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
 	golang.org/x/sync v0.7.0 // indirect
 )
 
 require (
+	github.com/luxedo/esb_fireplace-go v0.3.0
 	github.com/mattn/go-runewidth v0.0.15 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
 	golang.org/x/sys v0.17.0 // indirect
