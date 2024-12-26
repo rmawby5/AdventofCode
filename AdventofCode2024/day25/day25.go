242bc12b4665ca8f916b67b3f1c88ad72e7df48f
Ryan <rmawby52@gmail.com>
2024-12-26 18:57:25 +0000

    add d25 p1

---
+++ AdventofCode2024/day25/day25.go
@@ -0,0 +1,98 @@
+package day25
+
+import (
+	"time"
+
+	fileparse "Aoc.com/AdventOfCode2024/FileParse"
+)
+
+func comp(l []int, k []int) bool {
+	match := true
+	for i := range l {
+		if l[i]+k[i] > 7 {
+			match = false
+			break
+		}
+	}
+	return match
+}
+func conv(block []string) ([]int, string) {
+	heights := make([]int, len(block[0]))
+	var typ string
+	if block[0] == "#####" {
+		typ = "Lock"
+	} else {
+		typ = "Key"
+	}
+
+	if typ == "Lock" {
+		for i := 0; i < len(block[0]); i++ {
+			for j := 0; j < len(block); j++ {
+				//fmt.Println(string(block[j][i]))
+				if string(block[j][i]) != "#" {
+					heights[i] = j
+					break
+				}
+
+			}
+		}
+	} else if typ == "Key" {
+		for i := 0; i < len(block[0]); i++ {
+			for j := 0; j < len(block); j++ {
+				if string(block[j][i]) == "#" {
+					heights[i] = (len(block) - j)
+					break
+				}
+
+			}
+		}
+	}
+
+	return heights, typ
+}
+func Part1() (time.Duration, time.Duration, int) {
+	ParseStart := time.Now()
+	raw := fileparse.FileParse("day25/Input.txt")
+
+	ParseTime := time.Since(ParseStart)
+	P1Start := time.Now()
+
+	//Insert additional processsing here
+	var locks [][]int
+	var keys [][]int
+	var buffer []string
+	for r, i := range raw {
+		if len(i) != 0 {
+			buffer = append(buffer, i)
+
+		}
+		if len(i) == 0 || r == len(raw)-1 {
+			//fmt.Println(buffer)
+			//end of block reached, process heights
+			switch h, t := conv(buffer); t {
+			case "Key":
+				keys = append(keys, h)
+			case "Lock":
+				locks = append(locks, h)
+			}
+			buffer = nil
+		}
+
+	}
+	//fmt.Println(locks)
+	//fmt.Println("Keys : ")
+	//fmt.Println(keys)
+	//insert Puzzle solution here
+	sum := 0
+
+	for _, l := range locks {
+		for _, k := range keys {
+			if comp(l, k) {
+				sum++
+			}
+		}
+	}
+
+	P1Time := time.Since(P1Start)
+	return ParseTime, P1Time, sum
+}
--- AdventofCode2024/main.go
+++ AdventofCode2024/main.go
@@ -3,13 +3,13 @@ package main
 import (
 	"os"
 
-	"Aoc.com/AdventOfCode2024/day22"
+	"Aoc.com/AdventOfCode2024/day25"
 	"github.com/jedib0t/go-pretty/v6/table"
 )
 
 func main() {
-	Parse1Time, P1Time, P1Total := day22.Part1()
-	Parse2Time, P2Time, P2Total := day22.Part2()
+	Parse1Time, P1Time, P1Total := day25.Part1()
+	//Parse2Time, P2Time, P2Total := day22.Part2()
 	t := table.NewWriter()
 	t.SetOutputMirror(os.Stdout)
 	t.AppendHeader(table.Row{"Part", "Parse Time", "Part Time", "Result"})
@@ -17,7 +17,7 @@ func main() {
 		{1, Parse1Time, P1Time, P1Total},
 	})
 	t.AppendSeparator()
-	t.AppendRow([]interface{}{2, Parse2Time, P2Time, P2Total})
+	//t.AppendRow([]interface{}{2, Parse2Time, P2Time, P2Total})
 	t.Render()
 
 }
