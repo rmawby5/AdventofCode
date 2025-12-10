9cc74dfc2c6efb8530b04c0c4a5081dcc3a02697
Ryan <rmawby52@gmail.com>
2025-12-10 22:45:02 +0000

    Day 7 part 2- Swapped to modified BFS from p1, increased run speed

--- AdventofCode2025/day7/day7.go
+++ AdventofCode2025/day7/day7.go
@@ -20,13 +20,11 @@ func TachyonTrace(manifold [][]string, row int, pos int, cache map[string]int) i
 				branch += TachyonTrace(manifold, (row + 1), (pos + 1), cache)
 				branch += TachyonTrace(manifold, (row + 1), (pos - 1), cache)
 			}
-
 		} else {
 			branch = 1
 		}
 		cache[n] = branch
 	}
-
 	return cache[n]
 }
 
@@ -34,7 +32,6 @@ func StringAdd(v1 string, v2 string) string {
 	v1i, _ := strconv.Atoi(v1)
 	v2i, _ := strconv.Atoi(v2)
 	return strconv.Itoa(v1i + v2i)
-
 }
 
 func Part1() (time.Duration, time.Duration, int) {
@@ -45,7 +42,6 @@ func Part1() (time.Duration, time.Duration, int) {
 
 	ParseTime := time.Since(ParseStart)
 	//puzzle
-
 	startP1 := time.Now()
 	for i, j := range Input[0] {
 		if j == "S" {
@@ -72,7 +68,7 @@ func Part1() (time.Duration, time.Duration, int) {
 }
 
 func Part2() (time.Duration, time.Duration, int) {
-	cache := make(map[string]int)
+	//cache := make(map[string]int)
 	p2 := 0
 	//parse function
 	ParseStart := time.Now()
@@ -81,14 +77,48 @@ func Part2() (time.Duration, time.Duration, int) {
 	ParseTime := time.Since(ParseStart)
 	//puzzle
 	startP2 := time.Now()
-	var initialPos int
+	//var initialPos int
 	for i := range Input[0] {
 		if Input[0][i] == "S" {
-			initialPos = i
+			//initialPos = i
+			Input[1][i] = "1"
 		}
 	}
 
-	p2 = TachyonTrace(Input, 1, initialPos, cache)
+	for i := 1; i < len(Input)-1; i++ {
+		for j := 0; j < len(Input[i]); j++ {
+			if (Input[i][j] != ".") && (Input[i][j] != "^") {
+				switch Input[i+1][j] {
+				case ".":
+					Input[i+1][j] = Input[i][j]
+				case "^":
+					if Input[i+1][j+1] != "." {
+						//Tachyon beam already present
+						Input[i+1][j+1] = StringAdd(Input[i+1][j+1], Input[i][j])
+					} else {
+						Input[i+1][j+1] = Input[i][j]
+					}
+					if Input[i+1][j-1] != "." {
+						//Tachyon beam already present
+						Input[i+1][j-1] = StringAdd(Input[i+1][j-1], Input[i][j])
+					} else {
+						Input[i+1][j-1] = Input[i][j]
+					}
+				default:
+					Input[i+1][j] = StringAdd(Input[i+1][j], Input[i][j])
+				}
+			}
+		}
+	}
+
+	curval := "0"
+	for _, i := range Input[(len(Input) - 1)] {
+		if i != "." {
+			curval = StringAdd(curval, i)
+		}
+	}
+	p2, _ = strconv.Atoi(curval)
+	//p2 = TachyonTrace(Input, 1, initialPos, cache)
 
 	P2Time := time.Since(startP2)
 	return ParseTime, P2Time, p2
