4d030cddae7e9e52f11b12e13a8e8bfaf763185e
Ryan <rmawby52@gmail.com>
2025-12-12 15:18:34 +0000

    Day 12 part 1

---
+++ AdventofCode2025/day12/day12.go
@@ -0,0 +1,57 @@
+package day12
+
+import (
+	"strconv"
+	"strings"
+	"time"
+
+	fileparse "Aoc.com/AdventOfCode2025/FileParse"
+)
+
+func StrMultiply(v1 string, v2 string) int {
+	i1, _ := strconv.Atoi(v1)
+	i2, _ := strconv.Atoi(v2)
+	return (i1 * i2)
+}
+func Part1() (time.Duration, time.Duration, int) {
+	//parse function
+	ParseStart := time.Now()
+	Input := fileparse.FileParse("day12/Input.txt")
+
+	ParseTime := time.Since(ParseStart)
+	//puzzle
+	presentSize := []string{"7", "5", "7", "6", "7", "7"}
+	p1 := 0
+	startP1 := time.Now()
+	for _, i := range Input {
+		rowp := strings.Split(i, ": ")
+		gr := strings.Split(rowp[0], "x")
+		GridSize := StrMultiply(gr[0], gr[1])
+		reqSize := 0
+		presents := strings.Split(rowp[1], " ")
+		for j := range presents {
+
+			reqSize += StrMultiply(presentSize[j], presents[j])
+		}
+		if reqSize < GridSize {
+			p1++
+		}
+	}
+
+	P1Time := time.Since(startP1)
+	return ParseTime, P1Time, p1
+}
+
+func Part2() (time.Duration, time.Duration, int) {
+	//parse function
+	ParseStart := time.Now()
+	//Input := fileparse.FileParse("day11/Input.txt")
+
+	ParseTime := time.Since(ParseStart)
+	//puzzle
+	p2 := 0
+	startP2 := time.Now()
+
+	P2Time := time.Since(startP2)
+	return ParseTime, P2Time, p2
+}
--- AdventofCode2025/main.go
+++ AdventofCode2025/main.go
@@ -1,15 +1,22 @@
 package main
 
 import (
+	"fmt"
 	"os"
+	"strconv"
 
-	"Aoc.com/AdventOfCode2025/day11"
+	"Aoc.com/AdventOfCode2025/day12"
 	"github.com/jedib0t/go-pretty/v6/table"
 )
 
 func main() {
-	Parse1Time, P1Time, P1Total := day11.Part1()
-	Parse2Time, P2Time, P2Total := day11.Part2()
+	Parse1Time, P1Time, P1Total := day12.Part1()
+	Parse2Time, P2Time, P2Total := day12.Part2()
+	_, e1 := strconv.Atoi("1")
+	_, e2 := strconv.Atoi("e")
+	fmt.Println(e1)
+	fmt.Println(e2.Error())
+
 	t := table.NewWriter()
 	t.SetOutputMirror(os.Stdout)
 	t.AppendHeader(table.Row{"Part", "Parse Time", "Part Time", "Result"})
